ebe44fb47ec84a1cfe99791d27eb9a2699079afb
vagrant <vagrant@precise32.(none)>
2014-05-27 05:02:31 +0000

    Restrict Delete to only user who created it

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
 	def index
 		@places = Place.order(:name).page params[:page]
@@ -34,13 +34,18 @@ class PlacesController < ApplicationController
 		if @place.user != current_user
 			return render :text => 'Not Allowed', :status => :forbidden
 		end
-		
+
 		@place.update_attributes(place_params)
 		redirect_to root_path
 	end
 
 	def destroy
 		@place = Place.find(params[:id])
+
+			if @place.user != current_user
+				return render :text => 'Not Allowed', :status => :forbidden
+			end
+
 		@place.destroy
 		redirect_to root_path
 	end
