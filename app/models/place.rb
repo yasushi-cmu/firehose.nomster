bc4b0d2da99dd32e963dd37a604d8a20a49f029d
vagrant <vagrant@precise32.(none)>
2014-05-28 21:06:20 +0000

    Add validation to Address and Description

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,6 @@ class Place < ActiveRecord::Base
 	paginates_per 4
 	belongs_to :user
 	validates :name, :presence => true
+	validates :address, :presence => true, :length => { :minimum => 3}
+	validates :description, :presence => true
 end
